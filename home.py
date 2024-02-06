9640e5d1199c59175789797f55d208f95adaff68
Breno Ferreira <68168415+breno-pprf@users.noreply.github.com>
2024-02-06 08:20:55 -0300

    Add files via upload

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# functions
+
+st.set_page_config(layout='wide',
+                   page_title='Dashboard de Vendas',
+                   initial_sidebar_state='expanded')
+
+st.header('HOME TESTE')
+
+st.subheader('Página para armazenar e compartilhar estudos')
